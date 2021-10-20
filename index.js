70e0b3b2f304eec68e3f1aca5463ceee570e501d
DimitarKolev <s0568194@htw-berlin.de>
2021-10-20 17:18:25 +0200

    added env variables and updated models

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 // set up express app
 const app = express();
 
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => console.log('connected to db'))
+    .catch((err) => console.log(err));
+
+//first middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //initialize routes
 app.use('/api', require('./routes/api'));
+
+//error handling middleware
+app.use(function (err, req, res, next) {
+    res.status(422).send({ error: err._message });
+});
 
 //listen for requests
 app.listen(process.env.port || 4000, function() {
--- models/ninja.js
+++ models/ninja.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/*"geometry": {
+    "type": "Point",
+    "coordinates": [125.6, 10.1]
+  },*/
+
+//create geolocation schema
+/*const GeoSchema = new Shcema({
+    type: {
+        type: String,
+        default: "Point"
+    },
+    coordinates: {
+        type: [Number],
+        index: "2dsphere"
+    }
+}); */
+
 //create ninja Schema and model
 const NinjaSchema = new Schema({
     name: {
